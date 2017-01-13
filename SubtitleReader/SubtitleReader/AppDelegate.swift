fd4137727390a4983f1b81306f400c0a4d1f74e0
iljun lee <jun.lee@ftr.com>
2017-01-13 16:44:28 -0600

    initial workspace code with cocoa simple window

---
+++ SubtitleReader/SubtitleReader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SubtitleReader
+//
+//  Created by Jun Lee on 1/13/17.
+//  Copyright © 2017 Copyleft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SubtitleReader/SubtitleReader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SubtitleReader
+//
+//  Created by Jun Lee on 1/13/17.
+//  Copyright © 2017 Copyleft. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
